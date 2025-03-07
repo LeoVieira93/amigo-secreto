0465ce6ca4e4a09c4c07b278cf49a88874cf96aa
Leonardo Vieira <leonardovieira1993@gmail.com>
2025-03-07 20:03:17 -0300

    Implementação da lógica para criar uma lista de nomes e sortear um nome desta lista

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let nomes = [];
+
+function adicionarAmigo() {
+    const inputNome = document.getElementById("amigo");
+    const nome = inputNome.value.trim();
+
+    if(nome !== "") {
+        nomes.push(nome);
+        inputNome.value = "";
+
+        atualizarListaAmigos();
+    } else {
+        alert("Por favor, digite um nome válido!");
+    }
+}
+
+function atualizarListaAmigos() {
+    const listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+
+    nomes.forEach(function(amigo){
+        const li = document.createElement("li");
+        li.textContent = amigo;
+        listaAmigos.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if(nomes.length === 0) {
+        alert("Adicione pelo menos um amigo para sortear!");
+        return;
+    }
+
+    const indiceSorteado = Math.floor(Math.random() * nomes.length);
+    const amigoSorteado = nomes[indiceSorteado];
+
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li>O amigo sorteado é: ${amigoSorteado}</li>`
+}
+
